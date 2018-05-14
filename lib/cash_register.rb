064c0d98491da8b98e9c69f0c9ade7c5c8987069
Wasif Khan <waasif.khaan@gmail.com>
2018-05-14 01:34:59 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 require "pry"
 class CashRegister
   attr_accessor :cash_resister, :total, :last_transaction 
+=======
+class CashRegister
+  attr_accessor :cash_resister, :total
+>>>>>>> aeb141a3e33d2c81d06105187e60fa32d2992de9
   attr_reader :items
   def initialize(discount = 0)
   @total = 0
@@ -18,7 +23,10 @@ class CashRegister
     if quantity == 1 
       @total += price
       @items << title  
+<<<<<<< HEAD
       
+=======
+>>>>>>> aeb141a3e33d2c81d06105187e60fa32d2992de9
     else 
       @total = @total + price*quantity
       quantity.times do 
@@ -26,7 +34,10 @@ class CashRegister
     end 
     
     end 
+<<<<<<< HEAD
     @last_transaction = price*quantity
+=======
+>>>>>>> aeb141a3e33d2c81d06105187e60fa32d2992de9
   end  
   
   def apply_discount
@@ -39,9 +50,14 @@ class CashRegister
     "After the discount, the total comes to $#{@total.to_i}."
   end
   end 
+<<<<<<< HEAD
   def void_last_transaction 
     #binding.pry
     self.total = self.total - self.last_transaction
     
   end 
+=======
+ 
+  
+>>>>>>> aeb141a3e33d2c81d06105187e60fa32d2992de9
 end 
